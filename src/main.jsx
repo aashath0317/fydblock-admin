ed969deb01df98a6b52d97d379c658f2d1d8e7a4
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-09 11:18:02 +0530

    Add google login

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
 import './index.css'
 
+// Replace this with your actual Google Client ID from your .env file or console
+import.meta.env.VITE_GOOGLE_CLIENT_ID
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
